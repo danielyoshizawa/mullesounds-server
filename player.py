ca111a9f61366a2f1dd883dd3b3b1b838ee43eb2
danielyoshizawa <yoshidanielcwb@gmail.com>
2019-08-26 20:38:28 -0300

    Refactoring player to a class

--- player.py
+++ player.py
@@ -2,42 +2,59 @@ from playsound import playsound
 from os import listdir
 from os.path import isfile, join
 import json
+import sys
 
-# Reading the configuration file
-configFile = open("./config/configuration.txt")
-js = configFile.read()
+# Data structure for Configurations
+class Config:
+    path = ""
 
-obj = json.loads(js)
+class Player:
+    def __init__(self):
+        self.refreshSoundList()
 
-print(obj["networkAddress"])
+    # Loads the configuration file
+    def loadConfigFile(self):
+        obj = None
+        try:
+            configFile = open("./config/configuration.txt")
+            js = configFile.read()
+            obj = json.loads(js)
+            print(obj["networkAddress"])
+            print(js)
+            configFile.close()
+            config = Config()
+            config.path = obj["soundsPath"]
+            return config
+        except OSError:
+            print("Arquivo de configuracao nao encontrado")
+            sys.exit(1)
+        except IOError:
+            print("Não foi possivel ler o arquivo de configuração")
+            sys.exit(1)
 
-print(js)
 
-configFile.close()
+    # Loads the list of sounds
+    def loadSoundList(self, path):
+        soundList = [f for f in listdir(path) if (isfile(join(path, f)) and (f.endswith(".mp4") or f.endswith(".wav")))]
+        print(soundList)
+        soundListJson = {"sounds" : []}
 
-#path = '/Users/daniel/Documents/Development/Projects/mullesounds/sounds/'
+        for soundName in soundList:
+            soundStruct = {
+                "Filename": soundName
+            }
+            print(json.dumps(soundStruct))
+            soundListJson["sounds"].append(soundStruct)
 
-path = obj["soundsPath"]
+        print(json.dumps(soundListJson))
 
-soundList = [f for f in listdir(path) if (isfile(join(path, f)) and (f.endswith(".mp4") or f.endswith(".wav")))]
-print(soundList)
+        with open("./config/soundsList.txt", "w+") as writeFile:
+            json.dump(soundListJson, writeFile)
 
-soundListJson = {"sounds" : []}
+    def play(self, path, file):
+        playsound(join(path,file))
 
-for soundName in soundList:
-    soundStruct = {
-        "Filename": soundName
-    }
-    print(json.dumps(soundStruct))
-    soundListJson["sounds"].append(soundStruct)
-
-print(json.dumps(soundListJson))
-
-with open("./config/soundsList.txt", "w+") as writeFile:
-    json.dump(soundListJson, writeFile)
-
-def play(path, file):
-    playsound(join(path,file))
-
-for f in soundList:
-    play(path,f)
+    # Refreshs the list of Sounds
+    def refreshSoundList(self):
+        config = self.loadConfigFile()
+        self.loadSoundList(config.path)
